f1652cf3772d9d6abfdb9585f8e1bdc84eec223d
cpk0709 <cpk0709@gmail.com>
2022-11-25 00:22:16 +0900

    props로 전달

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,29 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2021, 2, 23),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    { id: "e3", title: "New TV", amount: 799.49, date: new Date(2021, 3, 2) },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Hi, my name is <span style={{ color: "pink" }}>phil choi</span>
         </p>
-        <ExpenseItem />
-        <ExpenseItem />
+        <ExpenseItem expenses={expenses} />
       </header>
     </div>
   );
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,18 +1,24 @@
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
-  const expenseDate = new Date(2022, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
+const ExpenseItem = ({ expenses }) => {
+  // const expenseDate = props.date;
+  // const expenseTitle = "Car Insurance";
+  // const expenseAmount = 294.67;
 
   return (
-    <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
-      <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
-      </div>
-    </div>
+    <>
+      {expenses.map((obj) => {
+        return (
+          <div className="expense-item">
+            <div>{obj.date.toISOString()}</div>
+            <div className="expense-item__description">
+              <h2>{obj.title}</h2>
+              <div className="expense-item__price">${obj.amount}</div>
+            </div>
+          </div>
+        );
+      })}
+    </>
   );
 };
 
