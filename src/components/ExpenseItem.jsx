f1dfd4d4754ca419cd12d467c76d8b6b0e9f5b27
cpk0709 <cpk0709@gmail.com>
2022-11-25 15:55:05 +0900

    날짜 수정

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,16 +1,21 @@
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ expenses }) => {
-  // const expenseDate = props.date;
-  // const expenseTitle = "Car Insurance";
-  // const expenseAmount = 294.67;
-
   return (
     <>
       {expenses.map((obj) => {
         return (
           <div className="expense-item">
-            <div>{obj.date.toISOString()}</div>
+            <div>
+              <div>{obj.date.getFullYear()}</div>
+              <div>{obj.date.toLocaleString("ko-KR", { month: "long" })}</div>
+              <div>{obj.date.toLocaleString("ko-KR", { day: "numeric" })}</div>
+            </div>
+            <div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
             <div className="expense-item__description">
               <h2>{obj.title}</h2>
               <div className="expense-item__price">${obj.amount}</div>
