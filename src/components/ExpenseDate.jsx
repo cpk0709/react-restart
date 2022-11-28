cee35f7e1e89c8c6deeade4f1e99ae5cd500928d
cpk0709 <cpk0709@gmail.com>
2022-11-28 17:40:55 +0900

    ExpenseDate css jsx 추

--- src/components/ExpenseDate.jsx
+++ src/components/ExpenseDate.jsx
@@ -1,12 +1,14 @@
+import "./ExpenseDate.css";
+
 const ExpenseDate = ({ date }) => {
   const year = date.getFullYear();
   const month = date.toLocaleString("ko-KR", { month: "long" });
   const day = date.toLocaleString("ko-KR", { day: "numeric" });
   return (
-    <div>
-      <div>{year}</div>
-      <div>{month}</div>
-      <div>{day}</div>
+    <div className="expense-date">
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__day">{day}</div>
     </div>
   );
 };
