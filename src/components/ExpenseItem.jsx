e04adc344e0988d3d69bf4ba1eb00fe8a62ce75d
cpk0709 <cpk0709@gmail.com>
2022-11-28 18:01:31 +0900

    더미 데이터 삭제

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -7,17 +7,7 @@ const ExpenseItem = ({ expenses }) => {
       {expenses.map((obj) => {
         return (
           <div className="expense-item">
-            {/* <div>
-              <div>{obj.date.getFullYear()}</div>
-              <div>{obj.date.toLocaleString("ko-KR", { month: "long" })}</div>
-              <div>{obj.date.toLocaleString("ko-KR", { day: "numeric" })}</div>
-            </div> */}
             <ExpenseDate date={obj.date} />
-            <div>
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
             <div className="expense-item__description">
               <h2>{obj.title}</h2>
               <div className="expense-item__price">${obj.amount}</div>
