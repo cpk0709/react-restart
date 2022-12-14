7516bfdda6dc23f1d0646ac8183efd572169ad78
cpk0709 <cpk0709@actbase.io>
2022-12-14 09:03:33 +0900

    ExpenseForm 추가

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,5 +1,25 @@
 const ExpenseForm = () => {
-  return <div>expense form</div>;
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__controls">
+          <label>Title</label>
+          <input type="text" />
+        </div>
+        <div className="new-expense__controls">
+          <label>Amount</label>
+          <input type="number" min="0.01" step="0.01" />
+        </div>
+        <div className="new-expense__controls">
+          <label>Date</label>
+          <input type="date" min="2019-01-01" step="2023-12-31" />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
 };
 
 export default ExpenseForm;
