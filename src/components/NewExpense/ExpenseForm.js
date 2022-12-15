daa323423a2b7c1aefeeb3e250138d59c5390361
cpk0709 <cpk0709@actbase.io>
2022-12-15 13:24:07 +0900

    class명 변경

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,15 +2,15 @@ const ExpenseForm = () => {
   return (
     <form>
       <div className="new-expense__controls">
-        <div className="new-expense__controls">
+        <div className="new-expense__control">
           <label>Title</label>
           <input type="text" />
         </div>
-        <div className="new-expense__controls">
+        <div className="new-expense__control">
           <label>Amount</label>
           <input type="number" min="0.01" step="0.01" />
         </div>
-        <div className="new-expense__controls">
+        <div className="new-expense__control">
           <label>Date</label>
           <input type="date" min="2019-01-01" step="2023-12-31" />
         </div>
