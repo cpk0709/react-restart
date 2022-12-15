be45792a557716cf4b0817119d58b98fa64318f2
cpk0709 <cpk0709@actbase.io>
2022-12-15 14:01:32 +0900

    amount date useState 추가

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,10 +1,23 @@
 import { useState } from "react";
+import moment from "moment";
 
 const ExpenseForm = () => {
   const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [date, setDate] = useState(moment().format("YYYY-MM-DD"));
+
   const titleChangeHandler = (event) => {
     setTitle(event.target.value);
   };
+
+  const amountChangeHandler = (event) => {
+    console.log(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
@@ -14,11 +27,23 @@ const ExpenseForm = () => {
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+            value={amount}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" step="2023-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            step="2023-12-31"
+            onChange={dateChangeHandler}
+            value={date}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
